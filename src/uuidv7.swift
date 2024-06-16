52f0c93200ffdc17cda0c16d415deba883e90d28
Prathamesh Kowarkar <hello@prathamesh.xyz>
2024-06-16 13:36:54 +0530

    swift: extend Foundation.UUID with v7 implementation

--- src/uuidv7.swift
+++ src/uuidv7.swift
@@ -1,26 +1,45 @@
 import Foundation
 
-func uuidv7() -> [UInt8] {
-    // random bytes
-    var value = (0..<16).map { _ in UInt8.random(in: 0...255) }
+extension UUID {
+    static func v7() -> Self {
+        // random bytes
+        var value = (
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255),
+            UInt8.random(in: 0...255)
+        )
 
-    // current timestamp in ms
-    let timestamp = Int(Date().timeIntervalSince1970 * 1000)
+        // current timestamp in ms
+        let timestamp: Int = .init(Date().timeIntervalSince1970 * 1000)
 
-    // timestamp
-    value[0] = UInt8((timestamp >> 40) & 0xFF)
-    value[1] = UInt8((timestamp >> 32) & 0xFF)
-    value[2] = UInt8((timestamp >> 24) & 0xFF)
-    value[3] = UInt8((timestamp >> 16) & 0xFF)
-    value[4] = UInt8((timestamp >> 8) & 0xFF)
-    value[5] = UInt8(timestamp & 0xFF)
+        // timestamp
+        value.0 = .init((timestamp >> 40) & 0xFF)
+        value.1 = .init((timestamp >> 32) & 0xFF)
+        value.2 = .init((timestamp >> 24) & 0xFF)
+        value.3 = .init((timestamp >> 16) & 0xFF)
+        value.4 = .init((timestamp >> 8) & 0xFF)
+        value.5 = .init(timestamp & 0xFF)
 
-    // version and variant
-    value[6] = (value[6] & 0x0F) | 0x70
-    value[8] = (value[8] & 0x3F) | 0x80
+        // version and variant
+        value.6 = (value.6 & 0x0F) | 0x70
+        value.8 = (value.8 & 0x3F) | 0x80
 
-    return value
+        return UUID(uuid: value)
+    }
 }
 
-let uuidVal = uuidv7()
-print(uuidVal.map { String(format: "%02x", $0) }.joined())
+let uuidVal: UUID = .v7()
+print(uuidVal)
