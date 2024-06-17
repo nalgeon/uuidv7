ef422964efc9bac64253b68c89d000e45485a94d
Brian Ewins <119598239+bewinsnw@users.noreply.github.com>
2024-06-17 20:42:15 +0100

    drop xxd dependency
    This version will run on onecompiler and should still be posixly correct. (ie bourne shell not bash)
    
    Instead of taking the first n bytes out of urandom and converting them to hex (using xxd) the trick here is to take bytes dropping any that are not in the range of allowed hex characters. This still ends up with the right number of hex digits, tho it's not particularly efficient.

--- src/uuidv7.sh
+++ src/uuidv7.sh
@@ -7,18 +7,18 @@ uuidv7() {
     timestamp_lo=$(( timestamp & 0xFF ))
 
     # 16 random bits (12 will be used)
-    rand_a=0x$(head -c 2 /dev/urandom|xxd -ps)
+    rand_a=0x$(LC_ALL=C tr -dc '0-9a-f' < /dev/urandom|head -c4)
     # ver is 0x7
-    ver_rand_a=$(( 0x7000 | ( 0xFFF & rand_a ) ))
+    ver_rand_a=$(( 0x7000 | ( 0xFFF & rand_a ) )) 
 
     # 16 random bits (14 will be used)
-    rand_b_1=0x$(head -c 2 /dev/urandom|xxd -ps)
+    rand_b_hi=0x$(LC_ALL=C tr -dc '0-9a-f' < /dev/urandom|head -c4)
     # var is 0b10
-    var_rand_b=$(( 0x8000 | ( 0x3FFF & rand_b_1 ) ))
+    var_rand_hi=$(( 0x8000 | ( 0x3FFF & rand_b_hi ) ))
     # remaining 48 bits of rand b
-    rand_b_2=$(head -c 6 /dev/urandom|xxd -ps)
+    rand_b_lo=$(LC_ALL=C tr -dc '0-9a-f' < /dev/urandom|head -c12)
 
-    printf "%08x-%04x-%04x-%4x-%s" "$timestamp_hi" "$timestamp_lo" "$ver_rand_a" "$var_rand_b" "$rand_b_2"
+    printf "%08x-%04x-%04x-%4x-%s" "$timestamp_hi" "$timestamp_lo" "$ver_rand_a" "$var_rand_hi" "$rand_b_lo"
 }
 
 for byte in $(uuidv7); do
