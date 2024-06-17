86e393b3c0d4abe5f1455cd6526f662a23fcbd88
Jim McKeeth <jim@mckeeth.org>
2024-06-17 20:27:13 +0200

    Update uuidv7.pas

--- src/uuidv7.pas
+++ src/uuidv7.pas
@@ -2,12 +2,9 @@ unit uuidv7;
 
 interface
 
-{.$DEFINE USE_MORMOT_RNG}
-
 uses
   SysUtils,
-  DateUtils {$IFDEF USE_MORMOT_RNG},
-  mormot.crypt.core {$ENDIF};
+  DateUtils;
 
 function GenerateUUIDv7: TGUID;
 
@@ -18,34 +15,20 @@ var
   timestamp: Int64;
   randomBytes: TBytes;
   uuid: TGUID;
-  {$IFDEF USE_MORMOT_RNG}
-  rng: TAESPRNG;
-  {$ELSE}
-  i: integer;
-  {$ENDIF}
 begin
   timestamp := DateTimeToUnix(Now) * 1000;
 
-  // Generate 10 cryptographically secure random bytes
+  // Generate 10 random bytes
   SetLength(randomBytes, 10);
-  {$IFDEF USE_MORMOT_RNG}
-  rng := TAESPRNG.Create;
-  try
-    rng.FillRandom(randomBytes, Length(randomBytes));
-  finally
-    rng.Free;
-  end;
-  {$ELSE}
-  for i := 0 to 9 do
+  Randomize;
+  for var i := 0 to 9 do
     randomBytes[i] := Random(256);
-  {$ENDIF}
 
-  // Populate the TUUID fields
-  uuid.D1 := (timestamp shr 28) and $FFFFFFFF;
-  uuid.D2 := (timestamp shr 12) and $FFFF;
-  uuid.D3 := ((timestamp and $0FFF) shl 4) or $7; // Set the version to 7
-
-  uuid.D4[0] := (randomBytes[0] and $3F) or $80; // Set the variant to 10xx
+  // Populate the TGUID fields
+  uuid.D1 := (timestamp shr 16) and $FFFFFFFF; // Top 32 bits of the 48-bit timestamp
+  uuid.D2 := ((timestamp shr 4) and $0FFF) or $7000; // Next 12 bits of the timestamp and version 7
+  uuid.D3 := ((timestamp and $0000000F) shl 12) or ((randomBytes[0] shr 4) and $0FFF); // Last 4 bits of timestamp and first 4 bits of randomBytes[0]
+  uuid.D4[0] := (randomBytes[0] and $0F) or $80; // Set the variant to 10xx
   Move(randomBytes[1], uuid.D4[1], 7);
 
   Result := uuid;
