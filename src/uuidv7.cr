e337abcf34948e4d61db01ea94d64a3867a6db78
Anton <m@antonz.org>
2024-06-18 22:55:39 +0500

    crystal: formatting

--- src/uuidv7.cr
+++ src/uuidv7.cr
@@ -9,16 +9,13 @@ class Uuidv7
 
   def initialize
     # random bytes
-    #
     value = @@rand.random_bytes(16)
 
     # current timestamp
-    #
     timestamp = Time.utc.to_unix_ms
     timestamp.unsafe_as(StaticArray(UInt8, 8)).reverse!.to_slice[2..].copy_to(value)
 
     # version and variant
-    #
     value[6] = (value[6] & 0x0F) | 0x70
     value[8] = (value[8] & 0x0F) | 0x80
 
