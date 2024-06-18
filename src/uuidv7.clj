56771b637599b80f21a61e5babfefc5855514aa3
Anton <m@antonz.org>
2024-06-18 22:49:54 +0500

    clojure: refactor

--- src/uuidv7.clj
+++ src/uuidv7.clj
@@ -4,19 +4,24 @@
 
 (defn gen-uuid-v7
   []
-  (let [rand-array (byte-array 10)
-        _ (.nextBytes (SecureRandom.) rand-array)
-        rand-seq (map byte rand-array)
-        uuid-seq (map #(str/lower-case (format "%02X" %))
-                      (concat
-                        ;; timestamp
-                        (map byte (.toByteArray (biginteger (bit-and (System/currentTimeMillis) 0xFFFFFFFFFFFF))))
-                        ;; ver
-                        (list (bit-or (bit-and (first rand-seq) 0x0F) 0x70))
-                        (list (second rand-seq))
-                        ;; var
-                        (list (bit-or (bit-and (first (drop 2 rand-seq)) 2r00111111) 2r10000000))
-                        (drop 3 rand-seq)))]
+  (let [rand-array (byte-array 10)]
+    (.nextBytes (SecureRandom.) rand-array)
+    (concat
+      ;; timestamp
+      (map byte (.toByteArray (biginteger (System/currentTimeMillis))))
+      ;; version
+      [(bit-or (bit-and (first rand-array) 0x0F) 0x70)]
+      [(nth rand-array 1)]
+      ;; variant
+      [(bit-or (bit-and (nth rand-array 2) 0x3F) 0x80)]
+      (drop 3 rand-array))))
+
+(defn uuid-to-string
+  [uuid-bytes]
+  (let [uuid-seq (map #(str/lower-case (format "%02X" %)) uuid-bytes)]
     (str/join "-" (map #(apply str %)
                        (let [indices (reductions + 0 [4 2 2 2 6])]
                          (map #(subvec (vec uuid-seq) %1 %2) (butlast indices) (rest indices)))))))
+
+(def uuid-bytes (gen-uuid-v7))
+(println (uuid-to-string uuid-bytes))
