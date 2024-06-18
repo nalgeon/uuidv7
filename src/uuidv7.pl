66d34f761af123d242a7ce764962aef61ff4f5b1
Scott Baker <scott@perturb.org>
2024-06-18 13:06:33 -0700

    Whitespace/indentation fixes

--- src/uuidv7.pl
+++ src/uuidv7.pl
@@ -30,33 +30,33 @@ sub uuidv7 {
 	my $type = shift() || "";
 
 	# Generate 16 random bytes (4 * 4)
-    my $uuid = "";
+	my $uuid = "";
 	for (my $i = 0; $i < 4; $i++) {
 		$uuid .= pack('I', int(rand(2 ** 32)));
 	}
 
-    # current timestamp in ms
-    my $timestamp = int(Time::HiRes::time() * 1000);
+	# current timestamp in ms
+	my $timestamp = int(Time::HiRes::time() * 1000);
 
-    # timestamp section
-    substr($uuid, 0, 1, chr(($timestamp >> 40) & 0xFF));
-    substr($uuid, 1, 1, chr(($timestamp >> 32) & 0xFF));
-    substr($uuid, 2, 1, chr(($timestamp >> 24) & 0xFF));
-    substr($uuid, 3, 1, chr(($timestamp >> 16) & 0xFF));
-    substr($uuid, 4, 1, chr(($timestamp >>  8) & 0xFF));
-    substr($uuid, 5, 1, chr($timestamp         & 0xFF));
+	# timestamp section
+	substr($uuid, 0, 1, chr(($timestamp >> 40) & 0xFF));
+	substr($uuid, 1, 1, chr(($timestamp >> 32) & 0xFF));
+	substr($uuid, 2, 1, chr(($timestamp >> 24) & 0xFF));
+	substr($uuid, 3, 1, chr(($timestamp >> 16) & 0xFF));
+	substr($uuid, 4, 1, chr(($timestamp >>  8) & 0xFF));
+	substr($uuid, 5, 1, chr($timestamp         & 0xFF));
 
-    # version and variant
-    substr($uuid, 6, 1, chr((ord(substr($uuid, 6, 1)) & 0x0F) | 0x70));
-    substr($uuid, 8, 1, chr((ord(substr($uuid, 8, 1)) & 0x3F) | 0x80));
+	# version and variant
+	substr($uuid, 6, 1, chr((ord(substr($uuid, 6, 1)) & 0x0F) | 0x70));
+	substr($uuid, 8, 1, chr((ord(substr($uuid, 8, 1)) & 0x3F) | 0x80));
 
 	# Hex string with no hypens
 	if ($type eq "hex") {
 		return bin2hex($uuid);
-	# Raw 16 byte string
+		# Raw 16 byte string
 	} elsif ($type eq "raw") {
 		return $uuid;
-	# String representation with hypens (default)
+		# String representation with hypens (default)
 	} else {
 		return uuid_format($uuid);
 	}
