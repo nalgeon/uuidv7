4d4e51a0534dfccdd472f92405ba00e8edd3116b
Anton <m@antonz.org>
2024-06-19 11:20:55 +0500

    perl: simplify

--- src/uuidv7.pl
+++ src/uuidv7.pl
@@ -3,33 +3,10 @@
 use v5.16;
 use Time::HiRes;
 
-###############################################################################
-###############################################################################
-
-printf("Raw   : %s\n", unpack("H*", uuidv7('raw')));
-printf("HEX   : %s\n", uuidv7('hex'));
-printf("String: %s\n", uuidv7('string'));
-
-###############################################################################
-###############################################################################
-
-sub bin2hex {
-	return unpack("H*", $_[0]);
-}
-
-sub uuid_format {
-	my $uuid  = shift();
-	my @parts = map { substr $uuid, 0, $_, '' } ( 4, 2, 2, 2, 6 );
-	my @hex   = map { bin2hex($_); } @parts;
-	my $ret   = join('-', @hex);
-
-	return $ret;
-}
-
 sub uuidv7 {
 	my $type = shift() || "";
 
-	# Generate 16 random bytes (4 * 4)
+	# 16 random bytes (4 * 4)
 	my $uuid = "";
 	for (my $i = 0; $i < 4; $i++) {
 		$uuid .= pack('I', int(rand(2 ** 32)));
@@ -38,7 +15,7 @@ sub uuidv7 {
 	# current timestamp in ms
 	my $timestamp = int(Time::HiRes::time() * 1000);
 
-	# timestamp section
+	# timestamp
 	substr($uuid, 0, 1, chr(($timestamp >> 40) & 0xFF));
 	substr($uuid, 1, 1, chr(($timestamp >> 32) & 0xFF));
 	substr($uuid, 2, 1, chr(($timestamp >> 24) & 0xFF));
@@ -50,16 +27,8 @@ sub uuidv7 {
 	substr($uuid, 6, 1, chr((ord(substr($uuid, 6, 1)) & 0x0F) | 0x70));
 	substr($uuid, 8, 1, chr((ord(substr($uuid, 8, 1)) & 0x3F) | 0x80));
 
-	# Hex string with no hypens
-	if ($type eq "hex") {
-		return bin2hex($uuid);
-	# Raw 16 byte string
-	} elsif ($type eq "raw") {
-		return $uuid;
-	# String representation with hypens (default)
-	} else {
-		return uuid_format($uuid);
-	}
+	return $uuid;
 }
 
-# vim: tabstop=4 shiftwidth=4 noexpandtab autoindent softtabstop=4
+my $uuid_val = uuidv7('hex');
+printf(unpack("H*", $uuid_val));
