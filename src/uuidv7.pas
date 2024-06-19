869deeb05e9c8c4c4961b1d44cb8358b7e79d77f
Jim McKeeth <jim@mckeeth.org>
2024-06-19 21:08:40 +0200

    Squashed commit of the following:
    commit e3c3bdfbf1be466ec38d214fb9b69c8a44f98de4
    Author: Jim McKeeth <jim@mckeeth.org>
    Date:   Wed Jun 19 21:05:30 2024 +0200
    
        Fixed some missing bits in the middle
    
    commit e300fe5ab9676710425d7e20b27b743f5e5f5bc8
    Author: Jim McKeeth <jim@mckeeth.org>
    Date:   Wed Jun 19 20:21:00 2024 +0200
    
        Sync with Main
    
        increasing the accuracy of the Delphi version to milliseconds

--- src/uuidv7.pas
+++ src/uuidv7.pas
@@ -13,14 +13,15 @@ implementation
 function GenerateUUIDv7: TGUID;
 var
   timestamp: Int64;
-  randomBytes: array [0..9] of Byte;
+  randomBytes: array[0..9] of Byte;
   uuid: TGUID;
-  I: Integer;
+  i: Integer;
 begin
+  FillChar(uuid, SizeOf(uuid), 0);
   {$IFDEF FPC}
-  timestamp := DateTimeToUnix(Now) * 1000; // seconds accuracy  
+  timestamp := DateTimeToUnix(Now) * 1000; // seconds accuracy
   {$ELSE}
-  timestamp := DateTimeToMilliseconds(Now) - Int64(UnixDateDelta + DateDelta) * MSecsPerDay; // millisecond accuracy 
+  timestamp := DateTimeToMilliseconds(Now) - Int64(UnixDateDelta + DateDelta) * MSecsPerDay; // millisecond accuracy
   {$ENDIF}
 
   // Generate 10 random bytes
@@ -28,12 +29,12 @@ begin
     randomBytes[i] := Random($FF);
 
   // Populate the TGUID fields
-  uuid.D1 := (timestamp shr 16) and $FFFFFFFF;        // Top 32 bits of the 48-bit timestamp
-  uuid.D2 := ((timestamp shr 4) and $0FFF) or $7000;  // Next 12 bits of the timestamp and version 7
-  uuid.D3 := ((timestamp and $0000000F) shl 12) or
-             ((randomBytes[0] shr 4) and $0FFF);      // Last 4 bits of timestamp and first 4 bits of randomBytes[0]
-  uuid.D4[0] := (randomBytes[0] and $0F) or $80;      // Set the variant to 10xx
-  Move(randomBytes[1], uuid.D4[1], 7);
+  uuid.D1 := (timestamp shr 16) and $FFFFFFFF;       // Top 32 bits of the 48-bit timestamp
+  uuid.D2 := ((timestamp shr 4) and $0FFF) or $7000; // Next 12 bits of the timestamp and version 7
+  uuid.D3 := ((timestamp and $0000000F) shl 12) or   // the last 4 bits of timestamp
+              (randomBytes[0] and $F0);              // the top 4 bits of randomBytes[0]
+  uuid.D4[0] := (randomBytes[0] and $0F) or $80;     // Set the variant to 10xx
+  Move(randomBytes[1], uuid.D4[1], 7);               // Remaining 7 bytes
 
   Result := uuid;
 end;
