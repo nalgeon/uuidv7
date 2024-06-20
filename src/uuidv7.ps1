634740c8682724424cc152b4b83da715b3a911af
lost <lost>
2024-06-21 00:40:58 +0800

    more simple version

--- src/uuidv7.ps1
+++ src/uuidv7.ps1
@@ -1,18 +1,12 @@
 function New-Uuidv7
 {
-  
   # random bytes
   $value = [byte[]]::new(16)
   [System.Random]::new().NextBytes($value)
 
   # current timestamp
   $timestamp = [DateTimeOffset]::UtcNow.ToUnixTimeMilliseconds()
-  $value[0] = ($timestamp -shr 40) -band 0xFF
-  $value[1] = ($timestamp -shr 32) -band 0xFF
-  $value[2] = ($timestamp -shr 24) -band 0xFF
-  $value[3] = ($timestamp -shr 16) -band 0xFF
-  $value[4] = ($timestamp -shr 8) -band 0xFF
-  $value[5] = $timestamp -band 0xFF
+  [System.BitConverter]::GetBytes($timestamp)[5..0].Copyto($value, 0)
 
   # version and variant
   $value[6] = ($value[6] -band 0x0F) -bor 0x70
