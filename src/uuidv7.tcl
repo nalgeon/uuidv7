31313df0c8c296fc5480b317454a2b791db32120
Anton <m@antonz.org>
2024-06-20 09:50:34 +0500

    tcl: comments

--- src/uuidv7.tcl
+++ src/uuidv7.tcl
@@ -5,17 +5,22 @@ namespace eval uuidv7 {
 }
 
 proc ::uuidv7::generate { } {
+    # random bytes
     set randomBytes {}
     for {set i 0} {$i < 16} {incr i} {
         lappend randomBytes [expr {int(rand() * 256)}]
     }
+
+    # current timestamp in ms
     set timestamp_ms [expr {[clock seconds] * 1000}]
 
+    # timestamp
     set timestamp_bytes {}
     for {set i 5} {$i >= 0} {incr i -1} {
         lappend timestamp_bytes [expr {($timestamp_ms >> ($i * 8)) & 0xFF}]
     }
 
+    # version and variant
     set bytes [lreplace $randomBytes 0 5 {*}$timestamp_bytes]
     lset bytes 6 [expr {([lindex $bytes 6] & 0x0F) | 0x70}]
     lset bytes 8 [expr {([lindex $bytes 8] & 0x3F) | 0x80}]
