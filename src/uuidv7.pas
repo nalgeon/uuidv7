3e80693790727b6b903f670573dfa8f2eaaf1ca6
skamradt <skamradt67@gmail.com>
2024-08-21 13:56:07 -0500

    Added ability to send an array of bytes for testing purposes and corrected an issue with the calculation of the var part of the guid.

--- src/uuidv7.pas
+++ src/uuidv7.pas
@@ -7,12 +7,12 @@ uses
   SysUtils, DateUtils;
 
 function GenerateUUIDv7: TGUID;
-function GenerateUUIDv7ex(const aTimestamp:int64):TGUID;
+function GenerateUUIDv7ex(const aTimestamp:int64;const aBytes:TBytes=[]):TGUID;
 
 implementation
 
 
-function GenerateUUIDv7ex(const aTimestamp:int64): TGUID;
+function GenerateUUIDv7ex(const aTimestamp:int64;const aBytes:TBytes): TGUID;
 var
   timestamp: Int64;
   randomBytes: array[0..9] of Byte;
@@ -21,15 +21,20 @@ var
 begin
   FillChar(uuid, SizeOf(uuid), 0);
 
-  // Generate 10 random bytes
-  for i := 0 to 8 do
-    randomBytes[i] := Random($FF);
+  // Generate 9 random bytes
+  if Length(aBytes) <> 10 then
+    begin
+      for i := 0 to 9 do
+        randomBytes[i] := Random($FF);
+    end
+  else
+    move(aBytes[0],RandomBytes[0],9);
 
   // Populate the TGUID fields
   uuid.D1 := (atimestamp shr 16) and $FFFFFFFF;      // Top 32 bits of the 48-bit timestamp
-  uuid.D2 := (atimestamp and $FFFF);         // Next 16 bits of the timestamp and version 7
-  uuid.D3 := (RandomBytes[8] SHL 4) or ((randomBytes[0] and $F0) shr 4) or $7000;
-  uuid.D4[0] := (randomBytes[0] and $0F) or $80;     // Set the variant to 10xx
+  uuid.D2 := (atimestamp and $FFFF);                 // Next 16 bits of the timestamp
+  uuid.D3 := (RandomBytes[8] SHL 4) or ((randomBytes[9] and $F0) shr 4) or $7000; // random bytes and version 7
+  uuid.D4[0] := (randomBytes[0] and $3F) or $80;     // Set the variant to 10xx
   Move(randomBytes[1], uuid.D4[1], 7);               // Remaining 7 bytes
 
   Result := uuid;
@@ -44,7 +49,7 @@ begin
   {$ELSE}
   timestamp := DateTimeToMilliseconds(Now) - Int64(UnixDateDelta + DateDelta) * MSecsPerDay; // millisecond accuracy
   {$ENDIF}
-  Result := GenerateUUIDv7ex(Timestamp);
+  Result := GenerateUUIDv7ex(Timestamp,[]);
 end;
 
 // Optionally remove this to make a regular unit for FPC too
