51cac86422428640d4b3a20fa414366cfd799525
Jim McKeeth <jim@mckeeth.org>
2024-09-28 14:49:34 +0200

    Update uuidv7.pas

--- src/uuidv7.pas
+++ src/uuidv7.pas
@@ -7,35 +7,34 @@ uses
   SysUtils, DateUtils;
 
 function GenerateUUIDv7: TGUID;
-function GenerateUUIDv7ex(const aTimestamp:Int64; const aBytes:TBytes=[]):TGUID;
+function GenerateUUIDv7ex(const Timestamp:Int64; const Bytes:TBytes=[]):TGUID;
 
 implementation
 
 
-function GenerateUUIDv7ex(const aTimestamp:Int64; const aBytes:TBytes): TGUID;
+function GenerateUUIDv7ex(const Timestamp:Int64; const Bytes:TBytes): TGUID;
 var
-  timestamp: Int64;
   randomBytes: array[0..9] of Byte;
   uuid: TGUID;
   i: Integer;
 begin
   FillChar(uuid, SizeOf(uuid), 0);
-  
-  if Length(aBytes) <> 10 then
+
+  if Length(Bytes) <> 10 then
   begin
     // Generate 10 random bytes
     for i := 0 to 9 do
       randomBytes[i] := Random($100);
   end
   else
-    move(aBytes[0],RandomBytes[0],10);
+    move(Bytes[0], RandomBytes[0],10);
 
   // Populate the TGUID fields
-  uuid.D1 := (atimestamp shr 16) and $FFFFFFFF;      // Top 32 bits of the 48-bit timestamp
-  uuid.D2 := (atimestamp and $FFFF);                 // Next 16 bits of the timestamp
-  uuid.D3 := (RandomBytes[8] SHL 4)
-       or ((randomBytes[9] and $F0) shr 4) or $7000; // random bytes and version 7
-  uuid.D4[0] := (randomBytes[0] and $3F) or $80;     // Set the variant to 10xx
+  uuid.D1 := (timestamp shr 16) and $FFFFFFFF;       // Top 32 bits of the 48-bit timestamp
+  uuid.D2 := ((timestamp shr 4) and $0FFF) or $7000; // Next 12 bits of the timestamp and version 7
+  uuid.D3 := ((timestamp and $0000000F) shl 12) or   // the last 4 bits of timestamp
+              (randomBytes[0] and $F0);              // the top 4 bits of randomBytes[0]
+  uuid.D4[0] := (randomBytes[0] and $0F) or $80;     // Set the variant to 10xx
   Move(randomBytes[1], uuid.D4[1], 7);               // Remaining 7 bytes
 
   Result := uuid;
