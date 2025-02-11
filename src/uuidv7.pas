39f9b9ace7eb7939aba31f0b12c34e3d9b3ac325
Jim McKeeth <jim@mckeeth.org>
2025-02-10 20:10:51 -0700

    pascal: fix off-by-one error and minor improvements (#35)
    Co-authored-by: Peter <errorsoft@mail.ru>
    Co-authored-by: skamradt <skamradt67@gmail.com>

--- src/uuidv7.pas
+++ src/uuidv7.pas
@@ -7,26 +7,27 @@ uses
   SysUtils, DateUtils;
 
 function GenerateUUIDv7: TGUID;
+function GenerateUUIDv7ex(const Timestamp:Int64; const Bytes:TBytes=[]):TGUID;
 
 implementation
 
-function GenerateUUIDv7: TGUID;
+
+function GenerateUUIDv7ex(const Timestamp:Int64; const Bytes:TBytes): TGUID;
 var
-  timestamp: Int64;
   randomBytes: array[0..9] of Byte;
   uuid: TGUID;
   i: Integer;
 begin
   FillChar(uuid, SizeOf(uuid), 0);
-  {$IFDEF FPC}
-  timestamp := DateTimeToUnix(Now) * 1000; // seconds accuracy
-  {$ELSE}
-  timestamp := DateTimeToMilliseconds(Now) - Int64(UnixDateDelta + DateDelta) * MSecsPerDay; // millisecond accuracy
-  {$ENDIF}
 
-  // Generate 10 random bytes
-  for i := 0 to 9 do
-    randomBytes[i] := Random($FF);
+  if Length(Bytes) <> 10 then
+  begin
+    // Generate 10 random bytes
+    for i := 0 to 9 do
+      randomBytes[i] := Random($100);
+  end
+  else
+    move(Bytes[0], RandomBytes[0],10);
 
   // Populate the TGUID fields
   uuid.D1 := (timestamp shr 16) and $FFFFFFFF;       // Top 32 bits of the 48-bit timestamp
@@ -37,6 +38,18 @@ begin
   Move(randomBytes[1], uuid.D4[1], 7);               // Remaining 7 bytes
 
   Result := uuid;
+end;
+
+function GenerateUUIDv7:TGUID;
+var
+  timestamp: Int64;
+begin
+  {$IFDEF FPC}
+  timestamp := DateTimeToUnix(Now) * 1000; // seconds accuracy
+  {$ELSE}
+  timestamp := DateTimeToMilliseconds(Now) - Int64(UnixDateDelta + DateDelta) * MSecsPerDay; // millisecond accuracy
+  {$ENDIF}
+  Result := GenerateUUIDv7ex(Timestamp,[]);
 end;
 
 // Optionally remove this to make a regular unit for FPC too
