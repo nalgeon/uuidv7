92ce605a14140ab878fc0b9c5e172cb4ba6a6005
Malthe Borch <mborch@gmail.com>
2025-09-24 11:34:15 +0200

    Implement Spark SQL for UUIDv7 generation
    Added Spark SQL query to generate UUIDv7.

--- src/uuidv7.sql
+++ src/uuidv7.sql
@@ -23,3 +23,16 @@ select
   lpad(to_hex((0x8000 + (random() * 0x3fff)::int)), 4, '0') || '-' ||
   -- rand_b
   lpad(to_hex((floor(random() * (2^48))::bigint >> 16)), 12, '0') AS value;
+
+-- Spark SQL
+select
+  -- timestamp
+  lpad(hex(((to_unix_timestamp(now()) * 1000)::bigint >> 16)), 8, '0') || '-' ||
+  lpad(hex(((to_unix_timestamp(now()) * 1000
+    + pmod(unix_timestamp(now()) * 1000, 1000)::bigint) & 65535)), 4, '0') || '-' ||
+  -- version / rand_a
+  lpad(hex((28672 + (random() * 4095)::int)), 4, '0') || '-' ||
+  -- variant / rand_b
+  lpad(hex((32768 + (random() * 16383)::int)), 4, '0') || '-' ||
+  -- rand_b
+  lpad(hex((floor(random() * power(2, 48))::bigint)), 12, '0') AS value;
