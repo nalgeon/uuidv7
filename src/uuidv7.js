f11545729e08e56426da5e8a3d5d3a18249c2ea4
Robin Pokorny <me@robinpokorny.com>
2025-11-01 14:26:43 +0100

    :cop: format JS code
    Removed unnecessary blank lines in uuidv7 function.

--- src/uuidv7.js
+++ src/uuidv7.js
@@ -2,19 +2,19 @@ function uuidv7() {
     const buffer = new ArrayBuffer(16);
     const view = new DataView(buffer);
     const value = new Uint8Array(buffer);
-    
+
     // current timestamp in ms
     const timestamp = Date.now();
     view.setUint16(0, Math.floor(timestamp / 2**32));
     view.setUint32(2, timestamp);
-    
+
     // random bytes
     crypto.getRandomValues(value.subarray(6));
-    
+
     // version and variant
     value[6] = (value[6] & 0x0f) | 0x70;
     value[8] = (value[8] & 0x3f) | 0x80;
-    
+
     return value;
 }
 
