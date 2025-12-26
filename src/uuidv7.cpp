35b3e91ba7133ee0ea83c6426e6eeaafd2519e05
Anton <m@antonz.org>
2025-12-26 16:09:36 +0500

    cpp: simplify

--- src/uuidv7.cpp
+++ src/uuidv7.cpp
@@ -1,3 +1,7 @@
+// UUIDv7 implementation in C++.
+// License: Public Domain.
+
+#include <algorithm>
 #include <array>
 #include <chrono>
 #include <cstdint>
@@ -5,26 +9,23 @@
 #include <random>
 
 std::array<uint8_t, 16> uuidv7() {
-    // random bytes
-    std::random_device rd;
-    std::array<uint8_t, 16> random_bytes;
-    std::generate(random_bytes.begin(), random_bytes.end(), std::ref(rd));
+    static thread_local std::mt19937 gen(std::random_device{}());
     std::array<uint8_t, 16> value;
-    std::copy(random_bytes.begin(), random_bytes.end(), value.begin());
+
+    // random bytes
+    std::generate(value.begin(), value.end(), std::ref(gen));
 
     // current timestamp in ms
     auto now = std::chrono::system_clock::now();
-    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(
-        now.time_since_epoch()
-    ).count();
+    auto ts = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 
     // timestamp
-    value[0] = (millis >> 40) & 0xFF;
-    value[1] = (millis >> 32) & 0xFF;
-    value[2] = (millis >> 24) & 0xFF;
-    value[3] = (millis >> 16) & 0xFF;
-    value[4] = (millis >> 8) & 0xFF;
-    value[5] = millis & 0xFF;
+    value[0] = static_cast<uint8_t>(ts >> 40);
+    value[1] = static_cast<uint8_t>(ts >> 32);
+    value[2] = static_cast<uint8_t>(ts >> 24);
+    value[3] = static_cast<uint8_t>(ts >> 16);
+    value[4] = static_cast<uint8_t>(ts >> 8);
+    value[5] = static_cast<uint8_t>(ts);
 
     // version and variant
     value[6] = (value[6] & 0x0F) | 0x70;
